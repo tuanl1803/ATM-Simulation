cbea811644dc1b203e0e06bdc6dd2f633f196dce
tuanl1803 <tuan_l1803@mitrais.com>
2019-11-19 16:11:19 +0700

    [StudyCase3] - Add junit

---
+++ src/test/java/com/mitrais/studycase/atm/services/AccountServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.mitrais.studycase.atm.services;
+
+import com.mitrais.studycase.atm.models.Account;
+import com.mitrais.studycase.atm.repos.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AccountServiceImplTest {
+
+    @Test
+    void findByAccountNumberAndPin_Empty() {
+        List<Account> accountList = new ArrayList<>();
+        AccountRepository accountRepositoryMock = mock(AccountRepository.class);
+
+        when(accountRepositoryMock.findByAccountNumberAndPin("empty","empty")).thenReturn(accountList);
+        AccountServiceImpl  accountService = new AccountServiceImpl();
+        assertEquals(accountService.findByAccountNumberAndPin("empty","empty"), accountList);
+    }
+
+    @Test
+    void findByAccountNumberAndPin_NotEmpty() {
+        List<Account> accountList =  Arrays.asList(new Account("John","123456", 100.0,"112233" ));
+        AccountRepository accountRepositoryMock = mock(AccountRepository.class);
+
+        when(accountRepositoryMock.findByAccountNumberAndPin("112233","123456")).thenReturn(accountList);
+        AccountServiceImpl  accountService = new AccountServiceImpl();
+        assertEquals(accountService.findByAccountNumberAndPin("112233","123456"), accountList);
+    }
+    @Test
+    void findByAccountNumber() {
+    }
+
+    @Test
+    void saveAccount() {
+    }
+}
---
+++ src/test/java/com/mitrais/studycase/atm/services/TransactionServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.mitrais.studycase.atm.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceImplTest {
+
+}
